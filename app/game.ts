1e247de2b2298d5121ea7c3ff77967a80ccda48a
Victor Akpan <v.akpan@live.com>
2021-04-25 12:47:55 +0400

    using modules

--- app/game.ts
+++ app/game.ts
@@ -1,10 +1,10 @@
-/// <reference path="utility.ts" />
-/// <reference path="result.ts" />
-/// <reference path="player.ts" />
-/// <reference path="scoreboard.ts" />
+import { getValue } from './utility';
+import { Result } from './result';
+import { Player } from './player';
+import { Scoreboard as ResultPanel } from './scoreboard';
 
-class Game {
-    private scoreboard: Scoreboard = new Scoreboard();
+export class Game {
+    private scoreboard: ResultPanel = new ResultPanel();
     // player: Player;
     // problemCount: number;
     // factor: number;
@@ -46,7 +46,7 @@ class Game {
     
         // loop through the text boxes and calculate the number that are correct
         for (let i = 1; i <= this.problemCount; i++) {
-          const answer: number = Number(Utility.getInputValue('answer' + i));
+          const answer: number = Number(getValue('answer' + i));
           if (i * this.factor === answer) {
             score++;
           }
